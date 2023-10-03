cd8a1020f5ca5317caecc501e7875a254bf321f2
Johan Leal <u20221205771@usco.edu.co>
2023-10-02 22:50:47 -0500

    unir

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-// Función para mostrar/ocultar el menú desplegable
-function toggleDropdown() {
-    var dropdownMenu = document.getElementById("dropdown-menu");
-    dropdownMenu.classList.toggle("show-menu");
+function toggleNotifications() {
+    var notificationMenu = document.getElementById("notification-menu");
+    notificationMenu.classList.toggle("show-menu");
 }
 
-// Agrega un listener al icono de elipsis para abrir/cerrar el menú
-var ellipsisIcon = document.getElementById("ellipsis-icon");
-ellipsisIcon.addEventListener("click", toggleDropdown);
+// Agrega un listener al icono de notificaciones para abrir/cerrar el menú
+var notificationIcon = document.getElementById("notification-icon");
+notificationIcon.addEventListener("click", toggleNotifications);
 
 // Cierra el menú si se hace clic fuera de él
-window.addEventListener("click", function(event) {
-    var dropdownMenu = document.getElementById("dropdown-menu");
-    if (event.target !== ellipsisIcon && !dropdownMenu.contains(event.target)) {
-        dropdownMenu.classList.remove("show-menu");
+document.addEventListener("click", function(event) {
+    var notificationMenu = document.getElementById("notification-menu");
+    if (!event.target.closest(".notification") && notificationMenu.classList.contains("show-menu")) {
+        notificationMenu.classList.remove("show-menu");
     }
 });
